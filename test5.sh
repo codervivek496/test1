848d27a8faa2f43d814e780f1a3694b49e27dfe2
codervivek496 <83624378+codervivek496@users.noreply.github.com>
2022-04-16 14:43:25 +0530

    Update test5.sh

--- test5.sh
+++ test5.sh
@@ -2,7 +2,7 @@ declare -A info
 read -p "Enter your first name: " first
 read -p "enter your last name: " last
 read -p "enter your mobile number: " mob
-read -p "enter you email ID" " email
+read -p "enter you email ID"  email
 
 info=(["first name"]=$first ["last name"]=$last ["Mobile num"]=$mob ["Email"]=$email)
 for bio in "${!info[@]}";
